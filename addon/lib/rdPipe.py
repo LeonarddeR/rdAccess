5fa74fbfa40938b8e2f3c1dbec664346082f21d4
Leonard de Ruijter <alderuijter@gmail.com>
2023-02-04 11:54:17 +0100

    Start with RdPipe built-in support

---
+++ addon/lib/rdPipe.py
@@ -0,0 +1,103 @@
+import winreg
+import os.path
+from enum import IntFlag, auto
+import addonHandler
+import platform
+import shlobj
+import atexit
+
+TS_ADD_INS_FOLDER = os.path.join("Software", "Microsoft", "Terminal Server Client", "Default", "AddIns")
+RD_PIPE_FOLDER_NAME = "RdPipe"
+RD_PIPE_FOLDER = os.path.join(TS_ADD_INS_FOLDER, RD_PIPE_FOLDER_NAME)
+NAME_VALUE_NAME = "Name"
+CHANNEL_NAMES_VALUE_NAME = "ChannelNames"
+CHANNEL_NAMES_VALUE_BRAILLE = "NVDA-BRAILLE"
+CHANNEL_NAMES_VALUE_SPEECH = "NVDA-SPEECH"
+
+
+class KeyComponents(IntFlag):
+	RD_PIPE_KEY = auto()
+	NAME_VALUE = auto()
+	CHANNEL_NAMES_VALUE_BRAILLE = auto()
+	CHANNEL_NAMES_VALUE_SPEECH = auto()
+	CHANNEL_NAMES_VALUE_UNKNOWN = auto()
+
+
+def keyExists(key, subKey: str) -> bool:
+	try:
+		with winreg.OpenKey(key, subKey, winreg.KEY_READ | winreg.KEY_WOW64_64KEY) as key:
+			return True
+	except FileNotFoundError:
+	    return False
+
+
+def getDllPath() -> str:
+	addon = addonHandler.getCodeAddon()
+	expectedPath = os.path.join(addon.path, "dll", f"rd_pipe_{platform.architecture}.dll")
+
+	if not os.path.isfile(expectedPath):
+		raise FileNotFoundError(expectedPath)
+	roamingAppData = shlobj.SHGetKnownFolderPath(shlobj.FolderId.ROAMING_APP_DATA)
+	localAppData = shlobj.SHGetKnownFolderPath(shlobj.FolderId.LOCAL_APP_DATA)
+	if expectedPath.startswith(roamingAppData):
+		expectedPath.replace(roamingAppData, "%appdata%")
+	elif expectedPath.startswith(localAppData):
+		expectedPath.replace(localAppData, "%localappdata%")
+	return expectedPath
+
+
+def addToRegistry(key, persistent: bool = False) -> KeyComponents:
+	expectedPath = getDllPath()
+	res = KeyComponents(0)
+	subKey = RD_PIPE_FOLDER
+	openFlags = winreg.KEY_READ | winreg.KEY_WRITE | winreg.KEY_WOW64_64KEY
+	needsSetValueForChannels = False
+	try:
+		handle = winreg.OpenKeyEx(key, subKey, 0, openFlags)
+	except FileNotFoundError:
+		handle = winreg.CreateKeyEx(key, subKey, 0, openFlags)
+		res |= KeyComponents.RD_PIPE_KEY
+	try:
+		path, regType = winreg.QueryValueEx(handle, NAME_VALUE_NAME)
+		expandedPath = winreg.ExpandEnvironmentStrings(path)
+		if (
+			expandedPath != winreg.ExpandEnvironmentStrings(expectedPath)
+			and not os.path.isfile(expandedPath)
+		):
+			raise FileNotFoundError
+	except FileNotFoundError:
+		path = None
+	if path is None:
+		winreg.SetValueEx(handle, NAME_VALUE_NAME, 0, winreg.REG_EXPAND_SZ, expectedPath)
+		res |= KeyComponents.NAME_VALUE
+	try:
+		channels, regType = winreg.QueryValueEx(handle, CHANNEL_NAMES_VALUE_NAME)
+		if not isinstance(channels, list) or regType != winreg.REG_MULTI_SZ:
+			raise FileNotFoundError
+	except FileNotFoundError:
+		channels = []
+		needsSetValueForChannels = True
+	if CHANNEL_NAMES_VALUE_SPEECH not in channels:
+		channels.append(CHANNEL_NAMES_VALUE_SPEECH)
+		res |= KeyComponents.CHANNEL_NAMES_VALUE_SPEECH
+		needsSetValueForChannels = True
+	if CHANNEL_NAMES_VALUE_BRAILLE not in channels:
+		channels.append(CHANNEL_NAMES_VALUE_BRAILLE)
+		res |= KeyComponents.CHANNEL_NAMES_VALUE_BRAILLE
+		needsSetValueForChannels = True
+	if len(channels) > 2:
+		res |= KeyComponents.CHANNEL_NAMES_VALUE_UNKNOWN
+	if needsSetValueForChannels:
+		winreg.SetValueEx(handle, CHANNEL_NAMES_VALUE_NAME, 0, winreg.REG_MULTI_SZ, channels)
+	if not persistent:
+		atexit.register(deleteFromRegistry, key, res)
+	return res
+
+
+def deleteFromRegistry(key, components: KeyComponents):
+	if (
+		KeyComponents.RD_PIPE_KEY & components
+		or KeyComponents.NAME_VALUE & components
+	):
+		winreg.DeleteKey(key, RD_PIPE_FOLDER)
+		return
