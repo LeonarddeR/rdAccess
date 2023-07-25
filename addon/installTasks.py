84b3b304ccb25f99207c38fb1473be48758d2290
Leonard de Ruijter <L.de.Ruijter@sevenp.nl>
2023-07-25 11:25:06 +0200

    Lint

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -8,7 +8,6 @@ if typing.TYPE_CHECKING:
 	from .lib import configuration
 	from .lib import rdPipe
 else:
-	import addonHandler
 	addon: addonHandler.Addon = addonHandler.getCodeAddon()
 	configuration = addon.loadModule("lib.configuration")
 	rdPipe = addon.loadModule("lib.rdPipe")
@@ -19,13 +18,14 @@ def onInstall():
 		if addon.name == "nvdaRd":
 			result = gui.messageBox(
 				# Translators: message asking the user to remove nvdaRd add-on
-				_("This add-on was previously called NVDA Remote Desktop. "
-				"You have an installed version of that add-on. "
-				"Would you like to update to RDAccess?"),
+				_(
+					"This add-on was previously called NVDA Remote Desktop. "
+					"You have an installed version of that add-on. "
+					"Would you like to update to RDAccess?"
+				),
 				# Translators: question title
 				_("Previous version detected"),
-				wx.YES_NO|wx.ICON_QUESTION,
-				gui.mainFrame
+				wx.YES_NO | wx.ICON_WARNING
 			)
 			if result == wx.YES:
 				addon.requestRemove()
